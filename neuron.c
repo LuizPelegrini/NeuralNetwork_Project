458e36c2a0d6a31afbdf18a2d9ba132062ea1e4d
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2015-11-24 02:27:17 -0200

    Add findNeuronWeightValues() function

--- neuron.c
+++ neuron.c
@@ -1,28 +1,23 @@
 #include "neuron.h"
 
-char* generateOutput(char** input, char** weight, char b, char wBias, int neuronQuantity){
+char generateOutput(char** input, char** weight, char b, char wBias){
     int result;
-    int i, j, k;
-    char* resultArray;
+    int i, j;
 
-    resultArray = (char*)malloc(neuronQuantity*sizeof(char));
 
-    for(k=0;k<neuronQuantity;k++){
-        result = 0;
+    result = 0;
 
-        for(i=0;i<ROWS;i++){
-            for(j=0;j<COLUMNS;j++){
-                result += (input[i][j]*weight[i][j]);
-            }
+    for(i=0;i<ROWS;i++){
+        for(j=0;j<COLUMNS;j++){
+            result += (input[i][j]*weight[i][j]);
         }
-
-        // Include Bias
-        result += (b*wBias);
-
-        resultArray[k] = activationFunction(result);
     }
 
-    return resultArray;
+    // Include Bias
+    result += (b*wBias);
+
+    return activationFunction(result);
+
 }
 
 char activationFunction(int result){
@@ -31,7 +26,7 @@ char activationFunction(int result){
     return 0;
 }
 
-void changeWeights(char** input, char** weight, char output, char desiredOutput){
+void changeWeights(char** input, WeightMatrix* weightElement, char output, char desiredOutput){
     int i, j;
     int error;
 
@@ -39,9 +34,46 @@ void changeWeights(char** input, char** weight, char output, char desiredOutput)
 
     for(i=0;i<ROWS;i++){
         for(j=0;j<COLUMNS;j++){
-            weight[i][j] = (char)(weight[i][j] + (error * TAX_APRENDIZAGEM * input[i][j]));
+            weightElement->weightMatrix[i][j] = (char)(weightElement->weightMatrix[i][j] + (error * TAX_APRENDIZAGEM * input[i][j]));
         }
     }
 
-    weightBias = (char)(weightBias + (error * TAX_APRENDIZAGEM * bias));
+    weightElement->weightBias = (char)(weightElement->weightBias + (error * TAX_APRENDIZAGEM * bias));
+}
+
+void findNeuronWeightValues(int count){
+    DigitNode *aux, *dirty;
+    WeightMatrix *aux2;
+    char result;
+    int clean;
+
+    aux2 = weightController->first;
+
+    do{
+        aux = controller->initial;
+        dirty = controller->ending;
+        clean = 0;
+
+        ///CORE
+        do{
+            result = generateOutput(aux->matrix, aux2->weightMatrix, bias, aux2->weightBias);
+
+            if(result != aux->desiredOutput[count]){
+                changeWeights(aux->matrix, aux2, result, aux->desiredOutput[count]);
+                dirty = aux;
+                aux = aux->next;
+            }else{
+                if(aux == dirty)
+                    clean = 1;
+                aux = aux->next;
+            }
+
+        }while(!clean);
+        ///END_CORE
+
+
+        count++;
+
+    }while((aux2 = aux2->next) != NULL);
+
 }
--- neuron.h
+++ neuron.h
@@ -7,8 +7,8 @@
 // Var to count how many eras is needed to solve the problem
 int era;
 
-char* generateOutput(char** input, char** weight, char b, char wBias, int neuronQuantity);
+char generateOutput(char** input, char** weight, char b, char wBias);
 char activationFunction(int result);
-void changeWeights(char** input, char** weight, char output, char desiredOutput);
-
+void changeWeights(char** input, WeightMatrix* weightElement, char output, char desiredOutput);
+void findNeuronWeightValues(int count);
 #endif // NEURON_H
